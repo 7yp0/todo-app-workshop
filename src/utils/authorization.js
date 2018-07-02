364c95e9885b9917557e5ad521d42142e182adda
mil <mil@spreadshirt.net>
2018-07-02 13:44:10 +0200

    dont save token in object in localstorage

--- src/utils/authorization.js
+++ src/utils/authorization.js
@@ -4,7 +4,7 @@ import localStore from 'store';
 export const LOCAL_STORAGE_KEY = 'TEST_TODO_APP';
 
 export function saveTokenToLocalStorage(token: string): boolean {
-  localStore.set(LOCAL_STORAGE_KEY, { token });
+  localStore.set(LOCAL_STORAGE_KEY, token);
 
   return true;
 }

d7e5bb3058a182e3babf3c6f7ec777eeacc83925
mil <mil@spreadshirt.net>
2018-07-03 11:48:40 +0200

    fix graphql

--- src/configures.js
+++ src/configures.js
@@ -9,21 +9,34 @@ import {
 import thunk from 'redux-thunk';
 import ApolloClient from 'apollo-client/ApolloClient';
 import { HttpLink } from 'apollo-link-http';
+import { setContext } from 'apollo-link-context';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import config from './config';
 import reducers, { type AppState } from './reducers';
-import graphqlFetch from './utils/graphqlFetch';
 import { runsInServer } from './utils/environment';
+import { getTokenFromLocalStorage } from './utils/authorization';
 
 const { graphqlUrl } = config;
 
+const httpLink = new HttpLink({
+  uri: graphqlUrl,
+});
+
+const authLink = setContext((_: any, { headers }: Object) => {
+  const token = getTokenFromLocalStorage();
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token || '',
+    },
+  };
+});
+
 export const configureClient = (): ApolloClient =>
   new ApolloClient({
-    link: new HttpLink({
-      uri: graphqlUrl,
-      graphqlFetch,
-    }),
+    link: authLink.concat(httpLink),
     cache: new InMemoryCache(),
     ssrMode: false,
     connectToDevTools: true,
